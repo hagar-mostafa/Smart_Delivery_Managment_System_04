a9bc7124797528ccc2bd83f8304551aa317d5273
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-23 03:09:03 +0200

    Part 01 — Theoretical Questions Q2

--- OOP_04/Program.cs
+++ OOP_04/Program.cs
@@ -21,7 +21,24 @@ namespace OOP_04
 
 
         #region Part 01 — Theoretical Questions Q2
+        //1. A 
+        /*
+        Abstract class : provide a blueprint with abstract and non abstrac members
+        iterface : provide a contract with only method signature (no impelementation)
+         */
 
+        //2.  B
+        /*
+        1. if i want to have only a signature of method and 
+        2.if i want the class to impelement many interfaces
+         class can not inherite from more than one abstract class but can impelement many and many interfaces
+         */
+
+        //3.  C
+        /*
+        1. No , only one abstract class
+        2.Yes it can impelement many interfaces
+         */
         #endregion
         static public void Main(string[] args)
         {
