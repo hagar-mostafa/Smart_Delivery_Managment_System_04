26b1545feb40a947df843b3e848f6ce59288ac7f
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-23 03:00:55 +0200

    Part 01 — Theoretical Questions Q1

--- OOP_04/Program.cs
+++ OOP_04/Program.cs
@@ -4,6 +4,25 @@ namespace OOP_04
 {
     public class Program
     {
+        #region Part 01 — Theoretical Questions Q1
+        //1. A 
+        /*
+         Abatraction is the process of hiding the impelementation details and showing only the essential features of an object to the user 
+         */
+
+        //2.  B
+        /*
+        1. it reduce complexity
+        2.improve maintability
+        3.enhance secuirty 
+        4.promotes flexability and reusability
+         */
+        #endregion
+
+
+        #region Part 01 — Theoretical Questions Q2
+
+        #endregion
         static public void Main(string[] args)
         {
 
