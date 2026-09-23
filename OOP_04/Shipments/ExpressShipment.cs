eac4a1c8e8092d8df8bf54eb3939a69c3c4c2bae
Hagar Mostafa <hagarmostafa580@gmail.com>
2026-09-23 03:43:54 +0200

    impelement EstimatedCost abstract method and member

---
+++ OOP_04/Shipments/ExpressShipment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_04.Shipments
+{
+    public class ExpressShipment : Shipment
+    {
+        private decimal _ExtraFee;
+        public decimal ExtraFee
+        {
+            get
+            {
+                return _ExtraFee;
+            }
+            set
+            {
+                if (_ExtraFee >= 0)
+                    _ExtraFee = value;
+            }
+        }
+        public ExpressShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination) : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            ExtraFee = _ExtraFee;
+        }
+        #region impelement EstimatedCost abstract method and member
+        public override decimal EstimatedCost
+        {
+            get
+            {
+                return DeliveryFee + (Weight * 5) + ExtraFee;
+            }
+        }
+        
+        public override void PrintShipment()
+
+        {
+            Console.WriteLine("=== Express Shipment ===");
+            Console.WriteLine($"Tracking Code  : {TrackingCode}");
+            Console.WriteLine($"Description    : {Description}");
+            Console.WriteLine($"Weight         : {Weight}");
+            Console.WriteLine($"Delivery Fee   : {DeliveryFee}");
+            Console.WriteLine($"Urgency Fee    : {ExtraFee}");
+            Console.WriteLine($"Destination    : {Destination.GetFullAddress()}");
+            Console.WriteLine($"Estimated Cost : {EstimatedCost}");
+        }
+        #endregion
+    }
+}
